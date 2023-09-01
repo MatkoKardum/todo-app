cd7efcebacbe558ffbbdc49ca6f44915b319289f
MatkoKardum <94291440+MatkoKardum@users.noreply.github.com>
2023-09-01 17:29:34 +0200

    Removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import TaskList from "./components/TaskList";
 import EditTask from "./components/EditTask";
 import TaskForm from "./components/TaskForm";
